bec5f654be261b2d7aa51b32804667ac8b147bf7
Richard Potter <richardpotter.au@gmail.com>
2023-08-02 16:14:14 +1000

    add comment center on gold coast

--- src/components/LeafletMap.js
+++ src/components/LeafletMap.js
@@ -4,12 +4,15 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { Icon } from "leaflet";
 // import * as parkData from "./data/skateboard-parks.json";
 
+// note the Leaflet library is hardcoded to render into a div with class 'leaflet-container'
+// so we can't use CSS Modules
+// instead we use a simple pure CSS file
 import "./LeafletMap.css";
 
 function LeafletMap() {
   return (
     <MapContainer
-      center={[45.4, -75.7]}
+      center={[-28.0, 153.4]}
       zoom={12}
       scrollWheelZoom={false}
       className="leaflet-container"
