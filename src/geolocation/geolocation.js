346513ec0de6aad1970b158e2c23ea89b951b939
Richard Potter <richardpotter.au@gmail.com>
2023-08-03 09:38:00 +1000

    implement fetch and some error handling

--- src/geolocation/geolocation.js
+++ src/geolocation/geolocation.js
@@ -3,7 +3,7 @@ import API_KEY from "./geolocation_key";
 // sample URL
 // https://geo.ipify.org/api/v2/country,city?apiKey=at_nwvlYhYKNuZ10ltmgl29dJTZ5ZUWq&ipAddress=8.8.8.8
 
-const API_URL = "https://geo.ipify.org/api/v2/country,city?=";
+const API_URL = "https://geo.ipify.org/api/v2/country,city?apiKey=xx";
 
 const buildUrl = (ipAddress) => {
   const url = `${API_URL}${API_KEY}&ipAddress=${ipAddress}`;
@@ -11,6 +11,17 @@ const buildUrl = (ipAddress) => {
   return url;
 };
 
-export function getCity(ipAddress) {
+export async function getCity(ipAddress) {
   const url = buildUrl(ipAddress);
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(
+        `geolocation request failed with error: ${response.status}`
+      );
+    }
+    console.log("response", response);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
