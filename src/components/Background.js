f1a8c5582a3fdcb5aae73f5c5afd270e149645c1
Richard Potter <richardpotter.au@gmail.com>
2023-08-02 15:20:40 +1000

    divide up background container show image

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,9 +1,20 @@
 import React from "react";
 
+import patternBgMobile from "../images/pattern-bg-mobile.png";
+
 import classes from "./Background.module.css";
 
 function Background() {
-  return <div className={classes.container}></div>;
+  return (
+    <div className={classes.container}>
+      <img
+        className={classes.img}
+        src={patternBgMobile}
+        alt="background pattern"
+      />
+      <div className={classes.map}></div>
+    </div>
+  );
 }
 
 export default Background;
