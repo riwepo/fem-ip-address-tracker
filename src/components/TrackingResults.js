aece63c07a7b4b04426c4574ccdf18bde734d643
Richard Potter <richardpotter.au@gmail.com>
2023-08-02 11:33:14 +1000

    put card around tracking results

--- src/components/TrackingResults.js
+++ src/components/TrackingResults.js
@@ -1,9 +1,15 @@
 import React from "react";
 
+import Card from "./ui/Card";
+
 import classes from "./TrackingResults.module.css";
 
 function TrackingResults() {
-  return <div>Tracking Results</div>;
+  return (
+    <Card className={classes.card}>
+      <div>Tracking Results</div>;
+    </Card>
+  );
 }
 
 export default TrackingResults;
