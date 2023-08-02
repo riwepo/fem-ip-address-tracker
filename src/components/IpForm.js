11bcc013abf586443a8dc98218246842d17b9587
Richard Potter <richardpotter.au@gmail.com>
2023-08-02 12:36:14 +1000

    styling the form

--- src/components/IpForm.js
+++ src/components/IpForm.js
@@ -2,12 +2,19 @@ import React from "react";
 
 import Card from "./ui/Card";
 
+import iconArrow from "../images/icon-arrow.svg";
+
 import classes from "./IpForm.module.css";
 
 function IpForm() {
   return (
     <Card className={classes.card}>
-      <div>IP Form</div>
+      <form className={classes.form}>
+        <input />
+        <button title="submit" className={classes.button}>
+          <img src={iconArrow} alt="arrow" />
+        </button>
+      </form>
     </Card>
   );
 }
