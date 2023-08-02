f5b18325a9daf4ea60181632ea34d9e4ccac65f3
Richard Potter <richardpotter.au@gmail.com>
2023-08-03 09:27:20 +1000

    add submit handler

--- src/components/IpForm.js
+++ src/components/IpForm.js
@@ -7,9 +7,13 @@ import iconArrow from "../images/icon-arrow.svg";
 import classes from "./IpForm.module.css";
 
 function IpForm() {
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log("submit");
+  };
   return (
     <Card className={classes.card}>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={submitHandler}>
         <input
           className={classes.input}
           placeholder="Search for any IP address or domain"
