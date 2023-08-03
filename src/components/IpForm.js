7f61ac652ea2c733db2cd7dba41801618625a599
Richard Potter <richardpotter.au@gmail.com>
2023-08-03 10:25:28 +1000

    all form does is return ip address

--- src/components/IpForm.js
+++ src/components/IpForm.js
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "./ui/Card";
-import { getCity } from "../geolocation/geolocation";
 
 import iconArrow from "../images/icon-arrow.svg";
 
 import classes from "./IpForm.module.css";
 
-function IpForm() {
-  const submitHandler = (event) => {
+function IpForm({ onSubmit }) {
+  const [enteredIp, setEnteredIp] = useState("");
+  const [isValid, setIsValid] = useState(false);
+
+  const submitHandler = async (event) => {
     const broadbeachLibrary = "123.103.192.10";
     event.preventDefault();
-    console.log("submit");
-    const result = getCity(broadbeachLibrary);
+    // const localIp = event.target.value;
+    const localIp = broadbeachLibrary;
+    if (localIp === "") {
+      setIsValid(false);
+      return;
+    }
+    setEnteredIp(localIp);
+    onSubmit(localIp);
   };
+
   return (
     <Card className={classes.card}>
       <form className={classes.form} onSubmit={submitHandler}>
