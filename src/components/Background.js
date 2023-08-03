ab28b6987849b614d8eeec49035e1291e2972c33
Richard Potter <richardpotter.au@gmail.com>
2023-08-03 16:06:13 +1000

    swap image media query

--- src/components/Background.js
+++ src/components/Background.js
@@ -3,17 +3,21 @@ import React from "react";
 import LeafletMap from "./LeafletMap";
 
 import patternBgMobile from "../images/pattern-bg-mobile.png";
+import patternBgDesktop from "../images/pattern-bg-desktop.png";
 
 import classes from "./Background.module.css";
 
 function Background({ center }) {
   return (
     <div className={classes.container}>
-      <img
-        className={classes.img}
-        src={patternBgMobile}
-        alt="background pattern"
-      />
+      <picture className={classes.picture}>
+        <source srcset={patternBgDesktop} media="(min-width: 768px)" />
+        <img
+          className={classes.img}
+          src={patternBgMobile}
+          alt="background pattern"
+        />
+      </picture>
       <LeafletMap center={center} />
     </div>
   );
