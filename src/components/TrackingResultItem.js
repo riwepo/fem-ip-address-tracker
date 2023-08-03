161a92ac65c2d687e7d2a9b4fa2c8bf9ff254d72
Richard Potter <richardpotter.au@gmail.com>
2023-08-04 08:35:43 +1000

    add seperator bars

--- src/components/TrackingResultItem.js
+++ src/components/TrackingResultItem.js
@@ -5,8 +5,11 @@ import classes from "./TrackingResultItem.module.css";
 function TrackingResultItem({ heading, value }) {
   return (
     <div className={classes.container}>
-      <p className={classes.heading}>{heading}</p>
-      <p className={classes.value}>{value}</p>
+      <div className={classes.separator}></div>
+      <div className={classes.contentContainer}>
+        <p className={classes.heading}>{heading}</p>
+        <p className={classes.value}>{value}</p>
+      </div>
     </div>
   );
 }
