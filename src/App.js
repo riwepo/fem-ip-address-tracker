795949e319900768b1618c5c0e915f7b2dbb960f
Richard Potter <richardpotter.au@gmail.com>
2023-08-04 11:36:41 +1000

    add error handling

--- src/App.js
+++ src/App.js
@@ -28,10 +28,15 @@ function App() {
 
   const asyncUpdateCity = async (ipAddress) => {
     const geoResult = await getCity(ipAddress);
-    const trackingResults = convertGeoResult(geoResult);
-    setTrackingResults(trackingResults);
-    const geoLatLong = getLatLong(geoResult);
-    setLatLong(geoLatLong);
+    let localTrackingResults;
+    if (geoResult.isOk) {
+      localTrackingResults = convertGeoResult(geoResult);
+      const geoLatLong = getLatLong(geoResult);
+      setLatLong(geoLatLong);
+    } else {
+      localTrackingResults = convertGeoResultError(geoResult);
+    }
+    setTrackingResults(localTrackingResults);
   };
 
   const updateCity = () => {
@@ -41,6 +46,13 @@ function App() {
   // update city on first load with null ip
   // will use ip of request to show our ip
   useEffect(updateCity, []);
+
+  function convertGeoResultError(geoResult) {
+    const errorResults = [
+      { heading: "An error occured", value: geoResult.error },
+    ];
+    return errorResults;
+  }
 
   function convertGeoResult(geoResult) {
     const formatLocation = (location) => {
